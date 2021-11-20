c7f2d4e8be69d560d64c4d758a900b8638230a1d
Daniel L Acevedo <dlacevedo@protonmail.com>
2021-11-20 22:37:21 +0100

    upload image

--- server.js
+++ server.js
@@ -1,15 +1,54 @@
-/*Crearem una API REST de resposta ràpida:
+const express = require('express');
+const app = express();
 
-Nivell 1
-- Exercici 1
+app.set('view engine', 'ejs');
+
+//app.use(express.json());
+
+app.listen(8000,function(){
+    console.log("server is running");
+});
+
+/*Nivell 1,Exercici 1:
 Crea un servidor amb Express 
 que retorni a una petició GET a l'endpoint /user un json amb el teu nom, edat i la url des d'on es fa la petició.
+*/
+app.get('/user', function(req,res){
+    res.json({
+        name:'Daniel',
+        edat: '31',
+        url: 'http://localhost:8000/user'
+    });
+});
 
+/*
 - Exercici 2
 Afegeix un endpoint /upload 
 per a pujar al servidor un arxiu de tipus png, jpg o gif 
 que retorni un missatge d'error en cas que l'extensió de l'arxiu no coincideixi amb aquestes.
+*/
+app.get('/upload', function(req,res){
+  res.render("upload");
+});
+const path = require('path');
+const multer = require('multer');
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb)=>{
+    cb(null, 'images');
+  },
+  filename: (req, file, cb)=>{
+    console.log(file);
+    cb(null,Date.now()+path.extname(file.originalname))
+  }
+})
+
+const upload = multer({storage: storage});
+
+app.post('/upload', upload.single('image'), (req,res)=>{
+  res.send("image uploaded");
+})
+/*
 Nivell 2
 - Exercici 1
 Creu un endpoint /time 
